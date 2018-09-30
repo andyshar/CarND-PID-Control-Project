d4b935379c7d536b50704e878ba777237a93bdc4
AndyShar <andyshar1216@msn.com>
2018-09-30 13:39:05 +0800

    [Add] target_speed

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,8 @@ int main()
   double init_speed_pid_kd = 1.0;
   speed_pid.Init(init_speed_pid_kp, init_speed_pid_ki, init_speed_pid_kd);
 
+  double target_speed = 40.0;
+
   h.onMessage([&pid, &speed_pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
