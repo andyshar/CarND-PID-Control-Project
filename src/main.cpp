9c05fe58824321e721d684e956baa40178ea3544
AndyShar <andyshar1216@msn.com>
2018-09-30 13:41:31 +0800

    [Modify] Change parameters

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main()
 
   double target_speed = 40.0;
 
-  h.onMessage([&pid, &speed_pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid, &speed_pid, &target_speed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
